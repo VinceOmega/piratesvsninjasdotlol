f73f55282df6828b9d811c59f3e3608cb74a7997
Larry Stanfield <vince.omega@gmail.com>
2020-11-29 22:52:57 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Priates vs Ninjas: Scurvy into Darkness | Larry Stanfield IFT 301-86951</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="css/landing-page.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav id="pvn-top-nav" class="navbar navbar-light bg-dark static-top">
+    <ul class="container nav">
+      <li class="nav-item"><a href="/">Home</a></li>
+      <li class="nav-item"><a href="/story">Story</a></li>
+      <li class="nav-item"><a href="/cast">Cast</a></li>
+      <li class="nav-item"><a href="/theater">Theater</a></li>
+      <li class="nav-item"><a href="/director">Director</a></li>
+      <li class="nav-item"><a href="/lore">Lore</a></li>
+      <li class="nav-item"><a href="/reviews">Reviews</a></li>
+    </ul>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-5">The Saga Continues</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <section class="intro text-center bg-sand padding-vert-10">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla nisl justo, vel luctus enim porta quis. Cras consequat, velit vel eleifend cursus, dolor magna efficitur quam, at rhoncus magna massa ac nibh. Sed euismod cursus enim et pharetra. Aenean sodales vulputate turpis in pretium. Nulla nec mauris nec arcu malesuada dictum. Duis a magna rhoncus, vestibulum tortor ut, pulvinar lectus. Fusce pretium quis nulla at efficitur. Morbi id metus id odio elementum commodo eu sed orci. Duis sollicitudin imperdiet venenatis. Sed pellentesque mauris sit amet blandit sollicitudin. Aliquam dictum lacus id lacus tempor sodales. Ut sapien massa, pharetra sed metus nec, dapibus vehicula justo. Nunc ultrices tempus arcu quis venenatis. Donec eget blandit ex. Pellentesque a commodo risus. Suspendisse feugiat accumsan diam, feugiat malesuada mi.
+
+            Nullam in sem tellus. Sed ultrices ante ut ante tempus rutrum. Fusce rhoncus massa at sapien semper, at malesuada libero fermentum. Phasellus vel placerat ex. Duis porta, ante eget dictum pulvinar, lorem odio tristique mi, sed laoreet sapien erat a nisi. Sed nec lectus porta, dapibus ex ac, interdum massa. Suspendisse maximus nibh justo, nec interdum ex accumsan at. Sed vel lorem ultrices, feugiat eros quis, laoreet est. Ut feugiat eleifend metus, eget efficitur nibh gravida blandit. Phasellus elementum imperdiet massa quis euismod.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquam ligula non nibh sodales, vel aliquam nisi semper. Morbi vel condimentum felis. Quisque ac nibh varius, fermentum metus molestie, varius libero. Aenean cursus, nisl at porttitor tincidunt, arcu arcu convallis quam, laoreet viverra sem lectus at nisi. Nullam ut risus eleifend, imperdiet odio ultricies, fringilla lacus. Sed in lectus sagittis, pharetra eros vitae, laoreet lacus. Quisque rhoncus sagittis consequat.
+
+            Praesent eleifend odio sed mauris ornare lobortis. Aenean vel ex vitae lacus ornare rhoncus. Maecenas eget nisl vitae sem fermentum tempor ut vel enim. Sed a arcu venenatis sapien euismod vehicula. Aliquam tempor orci vel velit pharetra fringilla. Phasellus id gravida sem. Sed ut pharetra risus. Quisque justo nulla, elementum eget ipsum vitae, congue venenatis nisl. Nam vestibulum blandit quam, ac lacinia tellus euismod eu. Nam sollicitudin odio ipsum, quis porttitor est ultricies id. Donec dapibus lacus nibh, ut facilisis tellus luctus nec. Nam accumsan non neque quis posuere. Mauris tempus congue velit a fermentum. Phasellus id ipsum et metus blandit gravida. Suspendisse a felis varius, fringilla purus ac, ultrices nisl.
+          </p>
+          <p>
+            Donec est est, ultrices vitae lacinia eget, varius sed orci. Morbi sed viverra enim, et auctor tellus. Nullam semper congue elementum. Ut ullamcorper imperdiet nibh, non viverra magna tincidunt ac. Sed porta metus sed enim feugiat, sed luctus purus tristique. Etiam quis diam tortor. Pellentesque efficitur nibh a dolor fermentum luctus.
+
+            Praesent sed risus eu nisi laoreet rhoncus. Pellentesque cursus orci vel aliquam rutrum. Aliquam pretium, eros scelerisque ultrices vulputate, nibh nisi cursus sem, nec vestibulum leo ex sit amet quam. Praesent aliquam dolor ut dui mollis, eu luctus mauris mattis. Praesent rhoncus pharetra enim ut pellentesque. Fusce sollicitudin nisi eu leo mattis facilisis. Donec id lorem fringilla, lacinia mauris ut, luctus velit. Mauris sed nunc felis. Duis porta ex ac metus dictum malesuada.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Image Showcases -->
+  <section class="showcase">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" data-herf="" style="background-image: url('img/story.jpg');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Our Story So Far</h2>
+          <p class="lead mb-0">Learn about the central conflict of the Pirate vs Ninjas&copy; world!</p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 text-white showcase-img" data-herf="" style="background-image: url('img/cast.jpg');"></div>
+        <div class="col-lg-6 my-auto showcase-text">
+          <h2>Meet Our Cast!</h2>
+          <p class="lead mb-0">Our leading men and women who bring the Pirates vs Ninjas&copy; world to life!</p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" data-herf="" style="background-image: url('img/theaters.jpg');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>In Theaters Soon!</h2>
+          <p class="lead mb-0">Pumped to see the movie, find out where to go!</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Testimonials -->
+  <section class="testimonials text-center bg-primary">
+    <div class="container">
+      <h2 class="mb-5">What people are saying...</h2>
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
+            <h5>Margaret E.</h5>
+            <p class="font-weight-light mb-0">"I love the ninjas! I'm a ninja gal"</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
+            <h5>Fred S.</h5>
+            <p class="font-weight-light mb-0">"When Davy said 'Arrrrg', I felt that."</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
+            <h5>Sarah W.</h5>
+            <p class="font-weight-light mb-0">"Maybe the real booty was the friends we made along the way!"</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action -->
+  <section class="call-to-action text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h2 class="mb-4">Subscribe to our mailing list to get all the Pirates vs Ninjas&copy; news!</h2>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <form>
+            <div class="form-row">
+              <div class="col-12 col-md-9 mb-2 mb-md-0">
+                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
+              </div>
+              <div class="col-12 col-md-3">
+                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="footer bg-dark">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+          <p class="text-muted small mb-4 mb-lg-0">Pirates vs Ninjas&copy;  2020. All Rights Reserved.</p>
+        </div>
+        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item mr-3">
+              <a href="#">
+                <i class="fab fa-facebook fa-2x fa-fw"></i>
+              </a>
+            </li>
+            <li class="list-inline-item mr-3">
+              <a href="#">
+                <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <i class="fab fa-instagram fa-2x fa-fw"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
